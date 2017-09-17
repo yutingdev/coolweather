7e23b22d5160120892d841cf3da48f3d62d2dc6e
Ting <yuyutinga@gmail.com>
2017-09-17 17:29:58 +0800

    weathor function

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by yuyutinga on 2017/9/2.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
